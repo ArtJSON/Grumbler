3544c881d4de0a43851386722ddf9d0d3e325fe5
ArtJSON <artur.pietrzak1@outlook.com>
2023-02-14 00:10:22 +0100

    Install sass, create sidebar placeholder

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,1 +1,9 @@
-export function Sidebar() {}
+import classes from "./Sidebar.module.scss";
+
+export function Sidebar() {
+  return (
+    <nav className={classes.sidebar}>
+      <div></div>
+    </nav>
+  );
+}
