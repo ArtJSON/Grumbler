abab339dbde49ee9c3b89ca39601c0935fbe916e
ArtJSON <artur.pietrzak1@outlook.com>
2023-02-13 03:50:04 +0100

    Create post routes

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -3,24 +3,31 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
 
 export const postRouter = createTRPCRouter({
-  getRecent: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany({
-      orderBy: {
-        createdAt: "desc",
-      },
-      take: 25,
-      include: {
-        _count: {
-          select: {
-            comments: true,
-            forwards: true,
-            postLikes: true,
+  getRecent: publicProcedure
+    .input(
+      z.object({
+        page: z.number().min(0),
+      })
+    )
+    .query(({ ctx, input: { page } }) => {
+      return ctx.prisma.post.findMany({
+        orderBy: {
+          createdAt: "desc",
+        },
+        skip: page * 25,
+        take: 25,
+        include: {
+          _count: {
+            select: {
+              comments: true,
+              forwards: true,
+              postLikes: true,
+            },
           },
         },
-      },
-    });
-  }),
-  getSecretMessage: protectedProcedure.query(({ ctx }) => {
+      });
+    }),
+  create: protectedProcedure.query(({ ctx }) => {
     return "you can now see this secret message!";
   }),
 });
