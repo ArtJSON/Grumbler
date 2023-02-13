2922df6e4a7b559743bfe9ce2da7cd7903f186c2
ArtJSON <artur.pietrzak1@outlook.com>
2023-02-13 22:21:54 +0100

    Create ThemeContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,21 +8,25 @@ import { api } from "../utils/api";
 import "../styles/globals.css";
 import { useState } from "react";
 import { ColorTheme } from "../types/ColorTheme";
+import React from "react";
+
+const ThemeContext = React.createContext({
+  theme: "light",
+  toggleTheme: () => {},
+});
 
 const MyApp: AppType<{
   session: Session | null;
-  cookieColorScheme: "dark" | "light";
-}> = ({
-  Component,
-  pageProps: { cookieColorScheme, session, ...pageProps },
-}) => {
-  const [colorScheme, setColorScheme] = useState<ColorTheme>(
+  cookieTheme: "dark" | "light";
+}> = ({ Component, pageProps: { cookieTheme, session, ...pageProps } }) => {
+  const [theme, setTheme] = useState<ColorTheme>(
     (getCookie("color-scheme") as ColorTheme) ?? "light"
   );
 
-  const toggleColorScheme = (value?: ColorTheme) => {
-    const nextColorTheme = value || (colorScheme === "dark" ? "light" : "dark");
-    setColorScheme(nextColorTheme);
+  const toggleTheme = (value?: ColorTheme) => {
+    const nextColorTheme = value || (theme === "dark" ? "light" : "dark");
+
+    setTheme(nextColorTheme);
     setCookie("color-theme", nextColorTheme, {
       maxAge: 60 * 60 * 24 * 30,
     });
@@ -30,7 +34,9 @@ const MyApp: AppType<{
 
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ThemeContext.Provider value={{ theme: theme, toggleTheme: toggleTheme }}>
+        <Component {...pageProps} />
+      </ThemeContext.Provider>
     </SessionProvider>
   );
 };
