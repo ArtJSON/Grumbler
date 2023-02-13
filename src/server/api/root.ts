d95638d8ee31395f472a7420f89c239473149807
ArtJSON <artur.pietrzak1@outlook.com>
2023-02-13 06:24:23 +0100

    Split postRouter

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,3 +1,4 @@
+import { commentRouter } from "./routers/comment";
 import { postRouter } from "./routers/post";
 import { createTRPCRouter } from "./trpc";
 
@@ -8,6 +9,7 @@ import { createTRPCRouter } from "./trpc";
  */
 export const appRouter = createTRPCRouter({
   post: postRouter,
+  comment: commentRouter,
 });
 
 // export type definition of API
