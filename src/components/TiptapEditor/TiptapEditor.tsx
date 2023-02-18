2e67fe46fb98ba32b4a41a2125b26f323c63e8e3
ArtJSON <artur.pietrzak1@outlook.com>
2023-02-18 19:43:06 +0100

    Create tiptap editor component

---
+++ src/components/TiptapEditor/TiptapEditor.tsx
@@ -0,0 +1,74 @@
+import { RichTextEditor, Link } from "@mantine/tiptap";
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import Underline from "@tiptap/extension-underline";
+import TextAlign from "@tiptap/extension-text-align";
+import CharacterCount from "@tiptap/extension-character-count";
+import styles from "./TiptapEditor.module.scss";
+import Placeholder from "@tiptap/extension-placeholder";
+
+export function TiptapEditor() {
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Underline,
+      TextAlign,
+      Placeholder.configure({ placeholder: "Say something more..." }),
+      CharacterCount.configure({
+        limit: 5000,
+      }),
+    ],
+    content: "<p>Hello World! 🌎️</p>",
+  });
+
+  return (
+    <RichTextEditor
+      editor={editor}
+      styles={{
+        content: {
+          maxHeight: 500,
+          overflowY: "scroll",
+        },
+      }}
+    >
+      <RichTextEditor.Toolbar sticky stickyOffset={0}>
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.Bold />
+          <RichTextEditor.Italic />
+          <RichTextEditor.Underline />
+          <RichTextEditor.Strikethrough />
+          <RichTextEditor.ClearFormatting />
+          <RichTextEditor.Code />
+        </RichTextEditor.ControlsGroup>
+
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.H1 />
+          <RichTextEditor.H2 />
+          <RichTextEditor.H3 />
+          <RichTextEditor.H4 />
+        </RichTextEditor.ControlsGroup>
+
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.Blockquote />
+          <RichTextEditor.Hr />
+          <RichTextEditor.BulletList />
+          <RichTextEditor.OrderedList />
+        </RichTextEditor.ControlsGroup>
+
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.Link />
+          <RichTextEditor.Unlink />
+        </RichTextEditor.ControlsGroup>
+
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.AlignLeft />
+          <RichTextEditor.AlignCenter />
+          <RichTextEditor.AlignJustify />
+          <RichTextEditor.AlignRight />
+        </RichTextEditor.ControlsGroup>
+      </RichTextEditor.Toolbar>
+
+      <RichTextEditor.Content />
+    </RichTextEditor>
+  );
+}
