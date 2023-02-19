337678849d8cd9fb9313535c5fa298e70b65646a
ArtJSON <artur.pietrzak1@outlook.com>
2023-02-19 21:57:23 +0100

    Create post component

---
+++ src/components/Post/Post.tsx
@@ -0,0 +1,62 @@
+import Image from "next/image";
+import Link from "next/link";
+import { User } from "tabler-icons-react";
+import styles from "./Post.module.scss";
+
+interface PostProps {
+  id: string;
+  createdAt: string;
+  userId: string;
+  userImage?: string;
+  displayName: string;
+  username: string;
+  content: string;
+  commentCount: number;
+  likesCount: number;
+  forwardCount: number;
+  viewCount: number;
+  hasExtendedContent: boolean;
+}
+
+export function Post({
+  id,
+  createdAt,
+  userImage,
+  displayName,
+  username,
+  content,
+  commentCount,
+  likesCount,
+  forwardCount,
+  viewCount,
+  hasExtendedContent,
+}: PostProps) {
+  return (
+    <Link href={`/post/${id}`} className={styles.post}>
+      <div className={styles.infoHeader}>
+        <div className={styles.userInfo}>
+          {userImage ? (
+            <Image src={userImage} alt="User image" width={32} height={32} />
+          ) : (
+            <User size={32} strokeWidth={2} color={"black"} />
+          )}
+          <div className={styles.namesContainer}>
+            <span>{displayName}</span>
+            <span>{username}</span>
+          </div>
+        </div>
+        <div className={styles.date}>{createdAt}</div>
+      </div>
+      <div className={styles.content}>{content}</div>
+      {hasExtendedContent && (
+        <div className={styles.readMore}>Read more...</div>
+      )}
+      <div className={styles.infoFooter}>
+        <span>{commentCount}</span>
+        <span>{likesCount}</span>
+        <span>{forwardCount}</span>
+        <span>{viewCount}</span>
+      </div>
+    </Link>
+  );
+}
