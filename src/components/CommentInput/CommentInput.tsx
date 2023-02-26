ab03b96b7cd4f38c673fac76243f9f1c6bc95701
ArtJSON <artur.pietrzak1@outlook.com>
2023-02-26 21:55:44 +0100

    Create comment input component

---
+++ src/components/CommentInput/CommentInput.tsx
@@ -0,0 +1,68 @@
+import { useForm, zodResolver } from "@mantine/form";
+import styles from "./CommentInput.module.scss";
+import { z } from "zod";
+import { api } from "../../utils/api";
+import { Textarea } from "@mantine/core";
+
+interface CommentInputProps {
+  postId: string;
+  onSubmit?: () => void;
+}
+
+export function CommentInput({ postId, onSubmit }: CommentInputProps) {
+  const commentCreateMutation = api.comment.comment.useMutation({
+    onSuccess: () => {
+      if (onSubmit) {
+        onSubmit();
+      }
+    },
+  });
+
+  const form = useForm({
+    validate: zodResolver(
+      z.object({
+        content: z
+          .string()
+          .min(3, "Comment must be at least 3 characters long!")
+          .max(320),
+      })
+    ),
+    initialValues: {
+      comment: "",
+    },
+  });
+
+  return (
+    <form
+      className={styles.commentInput}
+      onSubmit={form.onSubmit((values) => {
+        commentCreateMutation.mutate({
+          postId: postId,
+          comment: values.comment,
+        });
+      })}
+    >
+      <Textarea
+        withAsterisk
+        placeholder="Leave a comment..."
+        maxLength={320}
+        minRows={3}
+        autosize
+        styles={{
+          input: {
+            fontSize: 16,
+          },
+        }}
+        {...form.getInputProps("comment")}
+      />
+      <div className={styles.footer}>
+        <button className={styles.submitButton} type="submit">
+          Submit
+        </button>
+        <span className={styles.contentLength}>
+          {form.values.comment.length} / 320 characters
+        </span>
+      </div>
+    </form>
+  );
+}
