e8f48e80fc32b0b1ea4e500fc8d50a7a8cbd9a95
ArtJSON <artur.pietrzak1@outlook.com>
2023-02-26 21:58:25 +0100

    Add reactions to post detail page

--- src/pages/post/[postId].tsx
+++ src/pages/post/[postId].tsx
@@ -1,6 +1,8 @@
 import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
+import { CommentInput } from "../../components/CommentInput/CommentInput";
 import { PostDetailed } from "../../components/Post/PostDetailed/PostDetailed";
+import { PostReactionsFooter } from "../../components/Post/PostFragments/PostReactionsFooter/PostReactionsFooter";
 import { api } from "../../utils/api";
 import styles from "./PostPage.module.scss";
 
@@ -9,7 +11,7 @@ interface PostPagePropsType {
 }
 
 export default function PostPage({ postId }: PostPagePropsType) {
-  const { data: postData } = api.post.getById.useQuery({ id: postId });
+  const { data: postData, refetch } = api.post.getById.useQuery({ id: postId });
 
   return (
     <div className={styles.postPage}>
@@ -21,6 +23,22 @@ export default function PostPage({ postId }: PostPagePropsType) {
         content={postData?.content ?? ""}
         extendedContent={postData?.extendedContent ?? undefined}
       />
+      <PostReactionsFooter
+        likesCount={postData?._count.postLikes ?? 0}
+        commentsCount={postData?._count.comments ?? 0}
+        forwardsCount={postData?._count.forwards ?? 0}
+        viewsCount={postData?.views ?? 0}
+        onLikeClick={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        onCommentClick={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        onForwardClick={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
+      <CommentInput postId={postId} onSubmit={refetch} />
     </div>
   );
 }
