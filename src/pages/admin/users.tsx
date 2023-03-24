8b85076d13e546f5e055f21c721fe0b8c87f2840
ArtJSON <artur.pietrzak1@outlook.com>
2023-03-25 00:38:10 +0100

    Create handleSortClick function

--- src/pages/admin/users.tsx
+++ src/pages/admin/users.tsx
@@ -1,11 +1,32 @@
 import { Table } from "@mantine/core";
+import { useState } from "react";
 import { ArrowsSort, Triangle, TriangleInverted } from "tabler-icons-react";
 import styles from "./AdminPage.module.scss";
 
 export default function UsersPage() {
+  const [sortOption, setSortOption] = useState("usr-asc");
+
+  console.log(sortOption);
+
+  const handleSortCLick = (newSortOption: string) => {
+    if (newSortOption === sortOption.substring(0, 3)) {
+      if (sortOption.substring(4, 7) === "asc") {
+        setSortOption(newSortOption + "-des");
+      } else {
+        setSortOption(newSortOption + "-asc");
+      }
+    } else {
+      setSortOption(newSortOption + "-asc");
+    }
+  };
+
   return (
     <div className={styles.adminPage}>
-      <UserTable onReviewClick={() => {}} onSortClick={() => {}} users={[]} />
+      <UserTable
+        onReviewClick={() => {}}
+        onSortClick={handleSortCLick}
+        users={[]}
+      />
     </div>
   );
 }
