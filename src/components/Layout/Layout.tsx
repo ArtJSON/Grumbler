bf335895680294e94905d2b32ffe746a0ea1bc86
ArtJSON <artur.pietrzak1@outlook.com>
2023-03-27 22:50:13 +0200

    Style responsive layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,16 @@
-import { AppShell, Button, Container, Flex, Navbar, Text } from "@mantine/core";
+import {
+  AppShell,
+  Burger,
+  Button,
+  Container,
+  Flex,
+  Header,
+  MediaQuery,
+  Navbar,
+  Text,
+} from "@mantine/core";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { Brightness, Login } from "tabler-icons-react";
 import { useThemeContext } from "../ThemeManager/ThemeManager";
 import { MainLinks } from "./MainLinks/MainLinks";
@@ -7,12 +18,34 @@ import { MainLinks } from "./MainLinks/MainLinks";
 export function Layout({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   const theme = useThemeContext();
+  const [opened, setOpened] = useState(false);
+
+  useEffect(() => {
+    if (opened) {
+      setOpened(false);
+    }
+  }, [router.pathname]);
 
   return (
     <AppShell
       padding={0}
+      navbarOffsetBreakpoint="sm"
+      asideOffsetBreakpoint="sm"
+      styles={(theme) => ({
+        main: {
+          backgroundColor:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[8]
+              : theme.colors.gray[0],
+        },
+      })}
       navbar={
-        <Navbar width={{ base: 300 }} p="xs">
+        <Navbar
+          p="md"
+          hiddenBreakpoint="sm"
+          hidden={!opened}
+          width={{ sm: 300, lg: 300 }}
+        >
           <Navbar.Section>
             <Text size="xl">Grumbler</Text>
           </Navbar.Section>
@@ -38,14 +71,22 @@ export function Layout({ children }: { children: React.ReactNode }) {
           </Navbar.Section>
         </Navbar>
       }
-      styles={(theme) => ({
-        main: {
-          backgroundColor:
-            theme.colorScheme === "dark"
-              ? theme.colors.dark[8]
-              : theme.colors.gray[0],
-        },
-      })}
+      header={
+        <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+          <Header height={{ base: 50, sm: 0 }} p="md">
+            <div
+              style={{ display: "flex", alignItems: "center", height: "100%" }}
+            >
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                mr="xl"
+              />
+            </div>
+          </Header>
+        </MediaQuery>
+      }
     >
       {router.pathname.split("/")[1] === "admin" ? (
         <>{children}</>
