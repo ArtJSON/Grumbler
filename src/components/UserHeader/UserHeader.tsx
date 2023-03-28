42c4742e086d79a425d89cea814c7ac609122c41
ArtJSON <artur.pietrzak1@outlook.com>
2023-03-28 22:40:09 +0200

    Refactor user header

--- src/components/UserHeader/UserHeader.tsx
+++ src/components/UserHeader/UserHeader.tsx
@@ -1,8 +1,14 @@
+import {
+  Button,
+  Flex,
+  MediaQuery,
+  Stack,
+  Text,
+  useMantineTheme,
+} from "@mantine/core";
 import Image from "next/image";
 import Link from "next/link";
 import { Settings } from "tabler-icons-react";
-import { useThemeContext } from "../ThemeManager/ThemeManager";
-import styles from "./UserHeader.module.scss";
 
 interface UserHeaderProps {
   username: string;
@@ -31,49 +37,70 @@ export function UserHeader({
   isUserFollowing,
   isUserOwner,
 }: UserHeaderProps) {
-  const theme = useThemeContext();
+  const theme = useMantineTheme();
 
   return (
-    <div
-      className={`${styles.userHeader} ${
-        theme.theme === "dark" ? styles.dark : ""
-      }`}
+    <Stack
+      sx={(t) => ({
+        backgroundColor: t.colorScheme === "dark" ? t.colors.dark[6] : t.white,
+        borderRadius: t.radius.sm,
+        border: `0.0625rem solid ${
+          t.colorScheme === "dark" ? t.colors.dark[4] : t.colors.gray[4]
+        }`,
+        position: "relative",
+        overflow: "hidden",
+      })}
+      spacing="sm"
+      p={16}
     >
-      <div className={styles.infoImageContainer}>
-        <div className={styles.userInfo}>
-          <span className={styles.displayName}>
-            <span>{displayName}</span>
-            {isUserOwner && (
-              <Link
-                className={`${styles.option} ${styles.desktopOnly}`}
-                href="/settings"
+      <MediaQuery smallerThan="xs" styles={{ flexDirection: "column-reverse" }}>
+        <Flex justify="space-between" gap={16}>
+          <Stack spacing={16}>
+            <Stack spacing={4}>
+              <Flex align="center" gap={4}>
+                <Text size="xl" inline sx={{ overflowWrap: "anywhere" }}>
+                  {displayName}
+                </Text>
+                {isUserOwner && (
+                  <Link href="/settings">
+                    <Text sx={{ lineHeight: 0 }}>
+                      <Settings size={24} strokeWidth={2} />
+                    </Text>
+                  </Link>
+                )}
+              </Flex>
+              <Text c="dimmed" sx={{ overflowWrap: "anywhere" }}>
+                @{username}
+              </Text>
+              <Text c="dimmed">Since {joinedAt}</Text>
+            </Stack>
+            {!isUserOwner && (
+              <Button
+                sx={{ alignSelf: "flex-start" }}
+                onClick={onFollowClick}
+                variant={isUserFollowing ? "filled" : "outline"}
               >
-                <Settings size={24} strokeWidth={2} />
-              </Link>
+                {isUserFollowing ? "Following" : "Follow"}
+              </Button>
             )}
-          </span>
-          <span className={styles.username}>{username}</span>
-          <span className={styles.joinedAt}>Since {joinedAt}</span>
-          {!isUserOwner && (
-            <button className={styles.followButton} onClick={onFollowClick}>
-              {isUserFollowing ? "Following" : "Follow"}
-            </button>
-          )}
-        </div>
-        <Image
-          width={96}
-          height={96}
-          src={imageUrl}
-          alt="User image"
-          className={styles.image}
-        />
-      </div>
-      {bio && <div className={styles.bio}>{bio}</div>}
-      <div className={styles.followerInfo}>
-        <span className={styles.followers}>{followers} followers</span>
-        <span className={styles.following}>{following} following</span>
-        <span className={styles.posts}>{posts} posts</span>
-      </div>
-    </div>
+          </Stack>
+          <Image width={96} height={96} src={imageUrl} alt="User image" />
+        </Flex>
+      </MediaQuery>
+      {bio && <Text size="lg">{bio}</Text>}
+      <MediaQuery smallerThan="xs" styles={{ flexDirection: "column", gap: 4 }}>
+        <Flex gap={24}>
+          <Text c="dimmed">
+            Followers: <Text component="span">{followers}</Text>
+          </Text>
+          <Text c="dimmed">
+            Following: <Text component="span">{following}</Text>
+          </Text>
+          <Text c="dimmed">
+            Posts: <Text component="span">{posts}</Text>
+          </Text>
+        </Flex>
+      </MediaQuery>
+    </Stack>
   );
 }
