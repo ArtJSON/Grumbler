42b9763c6ca5564422421cdae5682e47a7d29d0f
ArtJSON <artur.pietrzak1@outlook.com>
2023-03-28 23:19:09 +0200

    Fix overflow

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,7 +2,12 @@ import { Flex, Loader as MantineLoader } from "@mantine/core";
 
 export function Loader() {
   return (
-    <Flex h="100vh" justify="center" align="center">
+    <Flex
+      h="100vh"
+      justify="center"
+      align="center"
+      sx={{ boxSizing: "border-box" }}
+    >
       <MantineLoader size={64} />
     </Flex>
   );
