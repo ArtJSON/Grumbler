170c522d416c594bdbf4179d05c8a7ba772b5643
ArtJSON <artur.pietrzak1@outlook.com>
2023-04-01 01:24:25 +0200

    Add admin panel route guard

--- src/components/RouteGuard.tsx
+++ src/components/RouteGuard.tsx
@@ -1,6 +1,8 @@
+import { notifications } from "@mantine/notifications";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { errorMessages } from "../utils/errorMessages";
 
 export default function RouteGuard({
   children,
@@ -17,7 +19,21 @@ export default function RouteGuard({
       (!Boolean(session.data?.user.username) ||
         !Boolean(session.data?.user.displayName))
     ) {
+      notifications.show({
+        message: "Set username and display name to continue",
+      });
       router.push("/settings");
+    }
+
+    if (
+      router.pathname.split("/")[1] === "admin" &&
+      session.data?.user.role !== "ADMIN"
+    ) {
+      notifications.show({
+        message: errorMessages.FORBIDDEN,
+        color: "red",
+      });
+      router.push("/");
     }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
